39d43a08f34676de80f710749cc1aa2310c11ecb
Sneh Pandya <sneh.pandya1@gmail.com>
2018-03-07 21:18:24 +0530

    Implement dummy database structure using Room
    If applied, this commit will:
    * Generate dummy database structure of User table with Room Architecture Component

---
+++ app/src/main/java/com/chintansoni/android/architecturecomponentsblueprint/model/database/entity/User.kt
@@ -0,0 +1,16 @@
+package com.chintansoni.android.architecturecomponentsblueprint.model.database.entity
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+/**
+ * Created by Sneh on 07-03-2018.
+ */
+@Entity(tableName = "user")
+data class User(@PrimaryKey(autoGenerate = true) var id: Long?,
+                @ColumnInfo(name = "name") var name: String,
+                @ColumnInfo(name = "email") var email: String
+) {
+    constructor() : this(null, "", "")
+}
